17039cdcda12e2efc6b89ea91b468e592dc3f935
LukaszDygon <ld13281@my.bristol.ac.uk>
2015-10-20 14:49:05 +0000

    adding 2 spheres and basic control

---
+++ Assets/Scripts/Player2Control.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Control : MonoBehaviour
+{
+    public float speed;
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal2");
+        float moveVertical = Input.GetAxis("Vertical2");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(speed * movement);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(speed * movement);
+    }
+}
