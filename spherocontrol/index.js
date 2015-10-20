d7117c47e12f7299244e5c0e3b3263965c0c4929
Jam <jamessewart@gmail.com>
2015-10-20 16:04:25 +0100

    abstract sphero management into a class, add examples use

--- spherocontrol/index.js
+++ spherocontrol/index.js
@@ -1,82 +1,20 @@
-let sphero = require('sphero');
+var log = console.log;
 
-let fs = require('fs');
-let log = console.log;
+var spheroManager = require('./spheroManager');
+log('Setting up manager');
+var manager = spheroManager();
 
-let connectedSpheros = {
-        instances: [],
-        deviceNames: []
-    },
-    desiredSpheros = 1;
 
-let spheroDeviceRegex = /tty\.Sphero.*/
-
-function updateSpheros() {
-    if (connectedSpheros.instances.length < desiredSpheros) {
-        fs.readdir('/dev/', (err, files) => {
-            if (err)
-                return log('[ERROR] in readdir:', err);
-            log('Searching for spheros...');
-            let unconnectedSpheros = files.filter(device => {
-                return device.match(spheroDeviceRegex);
-            }).filter(spheroDevice => {
-                return connectedSpheros.deviceNames.indexOf(spheroDevice) === -1;
-            });
-            if (unconnectedSpheros.length === 0)
-                log('None found');
-
-            for (let newSpheroDev of unconnectedSpheros) {
-                let spheroInstance = sphero('/dev/' + newSpheroDev);
-
-                let spheroConnectCallback = ((instance, deviceName) => {
-                    return (err) => {
-                        if (err) {
-                            log('[ERROR] in spheroOpen', err);
-                            log('        Trying again in 1 second');
-                            setTimeout(updateSpheros, 1000);
-                        } else {
-                            log('Succesfully connected', deviceName);
-                            setupSpheroInstance(instance, deviceName);
-                            connectedSpheros.instances.push(instance);
-                            connectedSpheros.deviceNames.push(deviceName);
-                        }
-                    }
-                })(spheroInstance, newSpheroDev);
-                log('Connecting to', newSpheroDev);
-                spheroInstance.connect(spheroConnectCallback);
-            }
-        });
-    }
-}
-
-function removeSphero(sphero, deviceName) {
-    connectedSpheros.instances.splice(sphero);
-    connectedSpheros.deviceNames.splice(deviceName);
-}
-
-function setupSpheroInstance(sphero, deviceName) {
-    sphero.on('error', (err) => {
-        log('[ERROR] in sphero', deviceName, '-', err);
-        // remove?
+log('Waiting for a sphero to connect');
+manager.onSpheroConnect(function(sphero) {
+    log('New sphero connected:', manager.names, manager.count);
+    sphero.newDataCallback(function(data) {
+        // log('New velocity:', data);
     });
-
-    sphero.on('close', () => {
-        log('Connection to', deviceName, 'closed');
-        removeSphero(sphero, deviceName);
-        updateSpheros();
-    });
-    // other events include:
-    //      ready, open, async, response, data
-    sphero.on("collision", function(data) {
-        console.log("collision detected");
-        console.log("  data:", data);
-
-        sphero.color("red");
-
-        setTimeout(function() {
-            sphero.color("green");
-        }, 1000);
-    });
-}
-
-updateSpheros();
+    var i = 0;
+    setInterval(function() {
+        i += 40;
+        sphero.force(i, 0.4);
+    }, 100);
+    // sphero.force(0, 0.1);
+});
