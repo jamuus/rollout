e5054eb9d7553f4c3e7a564d6bbe59031cebd15a
LukaszDygon <ld13281@my.bristol.ac.uk>
2015-10-22 12:18:47 +0100

    damage now depends on velocity of attacker

--- Assets/Scripts/CollisionAttack.cs
+++ Assets/Scripts/CollisionAttack.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class CollisionAttack : MonoBehaviour
 {
-    public int collisionAttackValue = 50;
+    public int collisionAttackValue = 20;
     public UniversalHealth health;
+	private float speed;
     void Start()
     {
-        health = GetComponent<UniversalHealth>();
-    }
+		health = GetComponent<UniversalHealth> ();
+		if (gameObject.name == "player1") {
+			speed = GetComponent<PlayerControl> ().speed;
+		} else if (gameObject.name == "player2") {
+			speed = GetComponent<Player2Control>().speed;
+		}
+	}
     void OnCollisionEnter(Collision col)
     {
 		if (col.gameObject.name == "player1" || col.gameObject.name == "player2")
 		{
-			health.damagePlayer (collisionAttackValue);
+			GameObject otherPlayer = col.gameObject;
+			health.damagePlayer ((int)(collisionAttackValue * otherPlayer.GetComponent<Rigidbody>().velocity.magnitude));
 		}
     }
 }
