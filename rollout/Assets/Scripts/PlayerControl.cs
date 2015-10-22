83630e382a1e7fd58c4dc6efcd83c71fae6f1758
LukaszDygon <ld13281@my.bristol.ac.uk>
2015-10-22 15:41:49 +0100

    fixed project structure

---
+++ rollout/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+	public Vector3 velocity;
+	public string horizontalAxis;
+	public string verticalAxis;
+	void Start()
+	{
+		velocity = GetComponent<Rigidbody> ().velocity;
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis(horizontalAxis);
+        float moveVertical = Input.GetAxis(verticalAxis);
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		velocity = rb.velocity;
+
+        rb.AddForce(speed * movement);
+    }
+}
