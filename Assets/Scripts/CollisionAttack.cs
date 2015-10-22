ccf0f168191e6359da22e4001eeccb5a61747cfa
LukaszDygon <ld13281@my.bristol.ac.uk>
2015-10-22 11:53:09 +0100

    added basic damage system

---
+++ Assets/Scripts/CollisionAttack.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionAttack : MonoBehaviour
+{
+    public int collisionAttackValue = 50;
+    public UniversalHealth health;
+    void Start()
+    {
+        health = GetComponent<UniversalHealth>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+		if (col.gameObject.name == "player1" || col.gameObject.name == "player2")
+		{
+			health.damagePlayer (collisionAttackValue);
+		}
+    }
+}
