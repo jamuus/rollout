dcdc1582283dab6c087443475ffca40cf3abbaa9
LukaszDygon <ld13281@my.bristol.ac.uk>
2015-10-22 14:02:26 +0100

    Added Particle system on Collision

--- Assets/Scripts/CollisionAttack.cs
+++ Assets/Scripts/CollisionAttack.cs
@@ -5,20 +5,24 @@ public class CollisionAttack : MonoBehaviour
 {
     public int collisionAttackValue = 10; //Base attack value
     private UniversalHealth health; // Access to UniversalHealth class
+	private ParticleSystem particles;
 	private Vector3 otherPlayerVel; // velocity of other player. Used for damage
 	private GameObject otherPlayer; // another Player's sphere
 	//private float speed;
     void Start()
     {
 		health = GetComponent<UniversalHealth> ();
+		particles = GetComponent<ParticleSystem> ();
 		if (gameObject.name == "player1") {
-			otherPlayer = GameObject.FindWithTag("Player2");
-			otherPlayerVel = otherPlayer.GetComponent<Player2Control>().velocity;
+			otherPlayer = GameObject.FindWithTag ("Player2");
+			otherPlayerVel = otherPlayer.GetComponent<Player2Control> ().velocity;
 		} else if (gameObject.name == "player2") {
-			otherPlayer = GameObject.FindWithTag("Player1");
-			otherPlayerVel = otherPlayer.GetComponent<PlayerControl>().velocity;
+			otherPlayer = GameObject.FindWithTag ("Player1");
+			otherPlayerVel = otherPlayer.GetComponent<PlayerControl> ().velocity;
 		}
+
 	}
+
 	void FixedUpdate()
 	{
 		if (gameObject.name == "player1" && otherPlayer) {
@@ -35,6 +39,7 @@ public class CollisionAttack : MonoBehaviour
 			int damage = (int)(collisionAttackValue * attackMagnitude);
 			health.damagePlayer(damage);
 			print(otherPlayer.name + " deals " + damage + " damage");
+			particles.Play ();
 		}
     }
 }
