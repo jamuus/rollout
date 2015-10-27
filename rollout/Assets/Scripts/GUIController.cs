dfd2e9ab233af127251094e16373abb5b80f416e
Dominic <domayre@outlook.com>
2015-10-27 15:45:08 +0000

    Make the health bars follow the spheros

--- rollout/Assets/Scripts/GUIController.cs
+++ rollout/Assets/Scripts/GUIController.cs
@@ -3,40 +3,35 @@ using System.Collections;
 
 public class GUIController : MonoBehaviour {
 
-    public GameObject player1;
-    public GameObject player2;
+    public GameObject player;
+    private LineRenderer lineRenderer;
 
     // Use this for initialization
     void Start()
     {
+        lineRenderer = (LineRenderer)GetComponent<LineRenderer>();
+        lineRenderer.SetVertexCount(2);
 
+        Color playerColour = player.name == "player1" ? Color.blue : Color.red;
+        lineRenderer.material.color = playerColour;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    void DrawQuad(Rect position, Color color)
-    {
-        Texture2D texture = new Texture2D(1, 1);
-        texture.SetPixel(0, 0, color);
-        texture.Apply();
-        GUI.skin.box.normal.background = texture;
-        GUI.Box(position, GUIContent.none);
-    }
-
-    void OnGUI()
-    {
         //Get the health of both players
-        int player1Health = player1.GetComponent<UniversalHealth>().currentHealth;
-        int player2Health = player2.GetComponent<UniversalHealth>().currentHealth;
+        int playerHealth = player.GetComponent<UniversalHealth>().currentHealth;
 
-        print("Player 1 Health = " + player1Health);
+        lineRenderer.SetPosition(0, player.transform.position + new Vector3(1, 1, -1));
+        lineRenderer.SetPosition(1, player.transform.position + new Vector3(1 + (10 * getPlayerHealth(player)), 1, -1));
+    }
 
-        //Draw the health bar for the players
-        DrawQuad(new Rect(20, 20, 200 * ((float)player1Health / UniversalHealth.maxHealth), 20), Color.blue);
-        DrawQuad(new Rect(20, 60, 200 * ((float)player2Health / UniversalHealth.maxHealth), 20), Color.red);
+    float getPlayerHealth(GameObject player)
+    {
+        //Get the absolute health
+        float playerHealth = player.GetComponent<UniversalHealth>().currentHealth;
+
+        //Work out the proportion
+        return playerHealth / UniversalHealth.maxHealth;
     }
 }
