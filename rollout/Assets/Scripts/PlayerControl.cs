d757adb2acdd9babe4a8b97fd069fcf9aa08d62a
pennicles <sfung94@gmail.com>
2015-10-29 15:39:46 +0000

    Fixed merge issues

--- rollout/Assets/Scripts/PlayerControl.cs
+++ rollout/Assets/Scripts/PlayerControl.cs
@@ -4,27 +4,14 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour
 {
     public float speed;
-<<<<<<< HEAD
     public Vector3 velocity;
     public string horizontalAxis;
     public string verticalAxis;
-    void Start()
-    {
-        velocity = GetComponent<Rigidbody> ().velocity;
-    }
-=======
-	public Vector3 velocity;
-	public string horizontalAxis;
-	public string verticalAxis;
-
-	//public Projectile projectile;
-   // private Vector3 projectilePosition;
 
 	void Start()
 	{
 		velocity = GetComponent<Rigidbody> ().velocity;
 	}
->>>>>>> refs/remotes/origin/Projectiles
 
     void FixedUpdate()
     {
@@ -37,21 +24,4 @@ public class PlayerControl : MonoBehaviour
         rb.AddForce(speed * movement);
 
     }
-
-	//public void Update()
-	//{
- //       if (Input.GetButtonDown ("Fire1"))
- //       {
- //           velocity = GetComponent<Rigidbody>().velocity;
-
- //           if (velocity.magnitude == 0)
- //           {
- //               velocity = new Vector3(0.5f, 0f, 0.5f);
- //           }
-
- //           projectilePosition = transform.position + velocity.normalized;
- //           var spawnedProjectile = (Projectile)Instantiate(projectile, projectilePosition, transform.rotation);
- //           spawnedProjectile.Initialise(velocity);
- //       }
-	//}
 }
