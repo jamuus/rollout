73aa528d70044a51c5c9cc9c54ab73816fd02efb
Jam <jamessewart@gmail.com>
2015-11-19 16:45:07 +0000

    basic integration complete, location is now computed in spheroLoc

--- spherocontrol/cvsphero/detectSphero.cpp
+++ spherocontrol/cvsphero/detectSphero.cpp
@@ -61,12 +61,16 @@ int sendToServer(int *fd, sockaddr_in *servaddr, spheroLoc loc)
     }
 }
 
+#define DEBUG
+
 int main(int, char**)
 {
-    int testing = 1;
+#ifndef DEBUG
     VideoCapture camera(0);
     if (!camera.isOpened())
         return -1;
+    namedWindow("Hough", 1);
+#endif
     vector<Vec3f> detectedOrangeCircles,
            detectedBlueCircles;
     Mat frame,
@@ -75,72 +79,73 @@ int main(int, char**)
         hsvImg,
         output;
 
-    namedWindow("Hough", 1);
 
     struct sockaddr_in serverAddress;
     int socketDescriptor;
-    spheroLoc sphero1 = {15, 50, 0};
+    spheroLoc sphero1 = {0, 0, 0};
 
     setupServerConnection(&serverAddress, &socketDescriptor);
 
     while (true) {
-        if (testing) {
-            sphero1.x = (sphero1.x + 1) % 100;
-            sendToServer(&socketDescriptor, &serverAddress, sphero1);
-            if (waitKey(25) == 27)  break;
-        } else {
-            camera >> frame;
+#ifdef DEBUG
+        // sphero1.x = ((sphero1.x + 1) % 20) - 10;
+        sendToServer(&socketDescriptor, &serverAddress, sphero1);
+        if (waitKey(1000) == 27)  break;
+#else
+        camera >> frame;
 
-            cvtColor(frame, hsvImg, CV_BGR2HSV);
-            inRange(hsvImg, cv::Scalar(0, 150, 100), cv::Scalar(20, 255, 255), orangeImage);
-            // needs adjusting
-            inRange(hsvImg, cv::Scalar(128, 100, 100), cv::Scalar(192, 255, 255), blueImage);
+        cvtColor(frame, hsvImg, CV_BGR2HSV);
+        inRange(hsvImg, cv::Scalar(0, 150, 100), cv::Scalar(20, 255, 255), orangeImage);
+        // needs adjusting
+        inRange(hsvImg, cv::Scalar(128, 100, 100), cv::Scalar(192, 255, 255), blueImage);
 
-            GaussianBlur(orangeImage, orangeImage, Size(11, 11), 5, 5);
-            GaussianBlur(blueImage, blueImage, Size(11, 11), 5, 5);
+        GaussianBlur(orangeImage, orangeImage, Size(11, 11), 5, 5);
+        GaussianBlur(blueImage, blueImage, Size(11, 11), 5, 5);
 
-            blueImage.copyTo(output);
+        blueImage.copyTo(output);
 
-            HoughCircles( orangeImage,
-                          detectedOrangeCircles,        // where to output the circles
-                          CV_HOUGH_GRADIENT,
-                          1,
-                          orangeImage.rows / 4,   // min dist between detected circles
-                          50,                     // edge threshold
-                          40,                     // hough space threshold
-                          0,
-                          0);
+        HoughCircles( orangeImage,
+                      detectedOrangeCircles,        // where to output the circles
+                      CV_HOUGH_GRADIENT,
+                      1,
+                      orangeImage.rows / 4,   // min dist between detected circles
+                      50,                     // edge threshold
+                      40,                     // hough space threshold
+                      0,
+                      0);
 
-            HoughCircles( blueImage,
-                          detectedBlueCircles,        // where to output the circles
-                          CV_HOUGH_GRADIENT,
-                          1,
-                          blueImage.rows / 4,   // min dist between detected circles
-                          50,                     // edge threshold
-                          40,                     // hough space threshold
-                          0,
-                          0);
+        HoughCircles( blueImage,
+                      detectedBlueCircles,        // where to output the circles
+                      CV_HOUGH_GRADIENT,
+                      1,
+                      blueImage.rows / 4,   // min dist between detected circles
+                      50,                     // edge threshold
+                      40,                     // hough space threshold
+                      0,
+                      0);
 
-            for (size_t i = 0; i < detectedOrangeCircles.size(); i++) {
-                Point center(cvRound(detectedOrangeCircles[i][0]),
-                             cvRound(detectedOrangeCircles[i][1]));
-                int radius = cvRound(detectedOrangeCircles[i][2]);
-                circle( output, center, 3, Scalar(0, 255, 0), -1, 8, 0 );
-                circle( output, center, radius, Scalar(0, 0, 255), 3, 8, 0 );
-            }
-
-            for (size_t i = 0; i < detectedBlueCircles.size(); i++) {
-                Point center(cvRound(detectedBlueCircles[i][0]),
-                             cvRound(detectedBlueCircles[i][1]));
-                int radius = cvRound(detectedBlueCircles[i][2]);
-                circle( output, center, 3, Scalar(0, 255, 0), -1, 8, 0 );
-                circle( output, center, radius, Scalar(0, 0, 255), 3, 8, 0 );
-            }
-
-            imshow("Hough Circles", output);
-            if (waitKey(25) == 27)  break;     // esc to quit, waits 25ms ~= 40fps
+        for (size_t i = 0; i < detectedOrangeCircles.size(); i++) {
+            Point center(cvRound(detectedOrangeCircles[i][0]),
+                         cvRound(detectedOrangeCircles[i][1]));
+            int radius = cvRound(detectedOrangeCircles[i][2]);
+            circle( output, center, 3, Scalar(0, 255, 0), -1, 8, 0 );
+            circle( output, center, radius, Scalar(0, 0, 255), 3, 8, 0 );
         }
+
+        for (size_t i = 0; i < detectedBlueCircles.size(); i++) {
+            Point center(cvRound(detectedBlueCircles[i][0]),
+                         cvRound(detectedBlueCircles[i][1]));
+            int radius = cvRound(detectedBlueCircles[i][2]);
+            circle( output, center, 3, Scalar(0, 255, 0), -1, 8, 0 );
+            circle( output, center, radius, Scalar(0, 0, 255), 3, 8, 0 );
+        }
+
+        imshow("Hough Circles", output);
+        if (waitKey(25) == 27)  break;     // esc to quit, waits 25ms ~= 40fps
+#endif
     }
+#ifndef DEBUG
     camera.release();
+#endif
     return 0;
 }
