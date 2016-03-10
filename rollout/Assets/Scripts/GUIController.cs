2570f9b9522ef73af76af9f1a994022f203a28e0
Dominic Ayre <domayre@outlook.com>
2016-03-10 12:31:35 +0000

    Prepared the game for building and fixed a couple of bugs

--- rollout/Assets/Scripts/GUIController.cs
+++ rollout/Assets/Scripts/GUIController.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
+//using UnityEditor;
 using System;
 
 public class GUIController : MonoBehaviour
