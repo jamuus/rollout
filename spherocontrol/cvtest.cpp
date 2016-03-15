01689850d80a88e996e318c091a1ea92bbd20ad8
Matthew Woodhouse <matthew@omegaminus.co.uk>
2016-03-15 10:51:21 +0000

    warp perspective

--- spherocontrol/cvtest.cpp
+++ spherocontrol/cvtest.cpp
@@ -3,23 +3,37 @@
 
 #include <opencv2/opencv.hpp>
 
+cv::Point2f warpPerspective(const cv::Point2f& p, const cv::Mat& m)
+{
+    cv::Point2f q(p.x * m.at<double>(0, 0) + p.y * m.at<double>(0, 1) + m.at<double>(0, 2),
+        p.x * m.at<double>(1, 0) + p.y * m.at<double>(1, 1) + m.at<double>(1, 2));
+
+    const double f = p.x * m.at<double>(2, 0) + p.y * m.at<double>(2, 1) + m.at<double>(2, 2);
+
+
+    q.x /= f;
+    q.y /= f;
+
+    return q;
+}
+
 int main()
 {
-    float width = 5.0f, height = 5.7f;
+    float width = 5.0f, height = 5.0f;
 
-    std::vector<cv::Point2f> corners { { 0.0f, 0.0f }, { 4.0f, 1.0f }, { 3.4f, 15.0f }, { -0.5f, 13.7f } };
+    // std::vector<cv::Point2f> corners { { 0.0f, 0.0f }, { 4.0f, 1.0f }, { 3.4f, 15.0f }, { -0.5f, 13.7f } };
+    std::vector<cv::Point2f> corners { { 0.0f, 0.0f }, { 4.0f, 0.0f }, { 4.0f, 4.0f }, { 0.0f, 4.0f } };
     std::vector<cv::Point2f> target { { 0, 0 }, { width, 0 }, { width, height }, { 0, height } };
 
     auto transform = cv::getPerspectiveTransform(corners, target);
 
     std::cout << transform << std::endl;
 
-    cv::Point3f p(3.5f, 1.765f, 1.0f);
+    cv::Point2f p(3.5f, 1.765f);
 
-    std::vector<cv::Point3f> points { p };
-    cv::transform(points, points, transform);
+    p = warpPerspective(p, transform);
 
-    std::cout << "\n" << points.front() << std::endl;
+    std::cout << "\n" << p << std::endl;
 
     return 0;
 }
--- spherocontrol/cvtest.js
+++ spherocontrol/cvtest.js
@@ -3,21 +3,36 @@
 var cv = require("opencv");
 
 var width = 5;
-var height = 5.7;
+var height = 5.0;
 
-var corners = [0,0,4,1,3.4,15,-0.5,13.7];
-var target = [0,0,width,0,width,height,0,height];
+//var corners = [0, 0, 4, 1, 3.4, 15, -0.5, 13.7];
+var corners = [0, 0, 4, 0, 4, 4, 0, 4];
+var target = [0, 0, width, 0, width, height, 0, height];
 
-var transform = new cv.Matrix.Eye(3,3).getPerspectiveTransform(corners, target);
+var transform = new cv.Matrix.Zeros(3, 3).getPerspectiveTransform(corners, target);
 
 console.log(transform.row(0));
 console.log(transform.row(1));
 console.log(transform.row(2));
 console.log();
 
-var points = [3.5,1.765,1];
-var out = transform.transform(points);
-console.log(cv);
+var point = [3.5, 1.765];
+
+console.log(warpPerspective(point, transform));
+
+function warpPerspective(p, m) {
+    var q = [
+        p[0] * m.get(0, 0) + p[1] * m.get(0, 1) + m.get(0, 2),
+        p[0] * m.get(1, 0) + p[1] * m.get(1, 1) + m.get(1, 2)
+    ];
+
+    var f = p[0] * m.get(2, 0) + p[1] * m.get(2, 1) + m.get(2, 2);
+
+    q[0] /= f;
+    q[1] /= f;
+
+    return q;
+}
 
 // var cv = require('opencv');
 //
