3fbb414cd8e7ef7780bfdd94750a4f17e0c81c41
Matthew Woodhouse <matthew@omegaminus.co.uk>
2016-04-25 18:47:52 +0100

    level

---
+++ rollout/Assets/Scripts/TextureScroll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TextureScroll : MonoBehaviour
+{
+    public Vector2 speed;
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        renderer.material.SetTextureOffset("_MainTex", Time.time * speed);
+    }
+}
