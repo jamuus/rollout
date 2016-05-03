42dbbb8b7738bb8c8f5a068e98a6b908766b9748
Lukasz Dygon <lukasz@Lukaszs-MacBook-Pro.local>
2016-05-03 11:05:01 +0100

    Implementing new Game Mode

---
+++ rollout/Assets/Scripts/RegionPoints.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegionPoints : Region {
+	private int[] points;
+
+	void Start() {
+		GameObject.FindGameObjectsWithTag("player");
+	}
+
+	public override void ApplyEffect(PlayerControl player)
+	{
+		
+	}
+}
---
+++ rollout/Assets/Scripts/regionHeal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegionHeal : Region {
+
+	public int magnitude = 0;
+
+	// Use this for initialization
+	public override void ApplyEffect(PlayerControl player)
+	{
+		if (Time.time > lastEffectCall + Rate)
+		{
+			player.GetComponent<UniversalHealth>().healPlayer(magnitude);
+			lastEffectCall = Time.time;
+		}
+	}
+
+}
